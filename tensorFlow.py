0134e9efd66cc7d6511e2c4f2c30ba91e4600465
kyuuuuuuuuuu <sageras.li@gmail.com>
2018-11-06 12:48:40 -0600

    no message

--- tensorFlow.py
+++ tensorFlow.py
@@ -19,26 +19,40 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 # test_images.shape
 # len(test_labels)
 
-plt.figure()
-plt.imshow(train_images[0])
-plt.colorbar()
-plt.grid(False)
+# plt.figure()
+# plt.imshow(train_images[0])
+# plt.colorbar()
+# plt.grid(False)
 # regularize the numbers in data from [0,255] to [0,1]
 train_images = train_images / 255.0
-
 test_images = test_images / 255.0
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
+# plt.figure(figsize=(10,10))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[train_labels[i]])
 
-model = keras.Sequential([
+# flatten 28*28 to 1*784
+keras_model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation=tf.nn.relu),
     keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
+# Compile the model
+keras_model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+keras_model.fit(train_images, train_labels, epochs=5) # acc 89.11%
+
+test_loss, test_acc = keras_model.evaluate(test_images, test_labels)
+
+print('Test accuracy:', test_acc) # acc 87.28%, overfitting
+
+predictions = keras_model.predict(test_images)
+
+np.argmax(predictions[0])
